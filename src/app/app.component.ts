e3bb78c251e852310174d2ce8fb8764c4ed08199
srigeetha <srigeethakanikanti@gmail.com>
2018-05-03 18:21:51 +0530

    All ngx components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component,TemplateRef } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { PageChangedEvent } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,95 @@ import { Component,TemplateRef } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  selected:string;
   open = false;
-  title:string="geetha";
+  isOpen=false;
+  title: string = "geetha";
+  items: any[] = [
+    'ajssd hkjashd kjas hdkjashd kjashd',
+    'ajssd hkjashd kjas hdkjashd kjashd',
+    'ajssd hkjashd kjas hdkjashd kjashd'
+  ];
+  //sortable
+  itemStringsLeft = [
+    'Windstorm',
+    'Bombasto',
+    'Magneta',
+    'Tornado'
+  ];
+ 
+  itemStringsRight = ['Mr. O', 'Tomato'];
+  //timepicker
+  mytime: Date = new Date();
+  //typeahead
+  selected: string;
+  states: string[] = [
+    'Alabama',
+    'Alaska',
+    'Arizona',
+    'Arkansas',
+    'California',
+    'Colorado',
+    'Connecticut',
+    'Delaware',
+    'Florida',
+    'Georgia',
+    'Hawaii',
+    'Idaho',
+    'Illinois',
+    'Indiana',
+    'Iowa',
+    'Kansas',
+    'Kentucky',
+    'Louisiana',
+    'Maine',
+    'Maryland',
+    'Massachusetts',
+    'Michigan',
+    'Minnesota',
+    'Mississippi',
+    'Missouri',
+    'Montana',
+    'Nebraska',
+    'Nevada',
+    'New Hampshire',
+    'New Jersey',
+    'New Mexico',
+    'New York',
+    'North Dakota',
+    'North Carolina',
+    'Ohio',
+    'Oklahoma',
+    'Oregon',
+    'Pennsylvania',
+    'Rhode Island',
+    'South Carolina',
+    'South Dakota',
+    'Tennessee',
+    'Texas',
+    'Utah',
+    'Vermont',
+    'Virginia',
+    'Washington',
+    'West Virginia',
+    'Wisconsin',
+    'Wyoming'
+  ];
+  //pagination
+  contentArray = new Array(90).fill('');
+  returnedArray: string[];
+
+  ngOnInit(): void {
+    this.contentArray = this.contentArray.map((v: string, i: number) => `Content line ${i + 1}`);
+    this.returnedArray = this.contentArray.slice(0, 10);
+  }
+
+  pageChanged(event: PageChangedEvent): void {
+    const startItem = (event.page - 1) * event.itemsPerPage;
+    const endItem = event.page * event.itemsPerPage;
+    this.returnedArray = this.contentArray.slice(startItem, endItem);
+  }
+  //rating
+  max: number = 10;
+  rate: number = 7;
+  isReadonly: boolean = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactusComponent } from './contactus/contactus.component';
@@ -7,7 +8,11 @@ import { ContactusComponent } from './contactus/contactus.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
-import { BsDropdownModule,AlertModule,BsDatepickerModule,ModalModule } from 'ngx-bootstrap';
+import {
+  AccordionModule, BsDropdownModule, AlertModule, ProgressbarModule, RatingModule, SortableModule,TooltipModule,TypeaheadModule,
+  PopoverModule, BsDatepickerModule, CarouselModule, ModalModule, PaginationModule, TabsModule, TimepickerModule
+} from 'ngx-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,11 +23,23 @@ import { BsDropdownModule,AlertModule,BsDatepickerModule,ModalModule } from 'ngx
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     BsDropdownModule.forRoot(),
     AlertModule.forRoot(),
     BsDatepickerModule.forRoot(),
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    AccordionModule.forRoot(),
+    CarouselModule.forRoot(),
+    PaginationModule.forRoot(),
+    PopoverModule.forRoot(),
+    ProgressbarModule.forRoot(),
+    RatingModule.forRoot(),
+    SortableModule.forRoot(),
+    TabsModule.forRoot(),
+    TimepickerModule.forRoot(),
+    TooltipModule.forRoot(),
+    TypeaheadModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent],
